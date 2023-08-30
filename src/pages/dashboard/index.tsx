0bfe703fe77cb0a9fc84b5d32de4f8fdab80e6d4
Fadli Hidayatullah <fadlihdytullah@outlook.com>
2023-08-30 10:55:39 +0700

    test log env
    Signed-off-by: Fadli Hidayatullah <fadlihdytullah@outlook.com>

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,6 +2,17 @@ import {Logo} from "@/components/common";
 import {withMeta} from "@/components/common/Meta/Meta";
 
 const DashboardPage = () => {
+  console.log(
+    "%c 🚀 process.env.NEXT_PUBLIC_SUPABASE_URL",
+    "color: green; font-weight: bold;",
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+  );
+
+  console.log(
+    "%c 🚀 process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY",
+    "color: green; font-weight: bold;",
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  );
   return (
     <div className="absolute inset-0 flex items-center justify-center">
       <div className="flex flex-col gap-3 text-center md:flex-row">
